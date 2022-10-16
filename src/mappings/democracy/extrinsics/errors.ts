67409cb2e6d61536663dbe64dcf9392101685d96
Gabriel <gabrieljaeger1@gmail.com>
2022-10-16 11:00:50 +0500

    improve error message

--- src/mappings/democracy/extrinsics/errors.ts
+++ src/mappings/democracy/extrinsics/errors.ts
@@ -1,5 +1,3 @@
-import { Delegation } from "../../../model/generated/delegation.model";
-
 export function TooManyOpenDelegations(block: number, wallet?: string) {
     return `Each wallet can only have one delegation at a time. 2 or more delegations are active for wallet ${wallet} at block ${block}`
 }
@@ -9,7 +7,7 @@ export function NoDelegationFound(block: number, wallet?: string) {
 }
 
 export function TooManyOpenVotes(block: number, index: number, wallet?: string) {
-    return `Each wallet can only have one active vote at a time. 2 or more votes are active for referendum ${index} and wallet ${wallet} at block ${block}`
+    return `Two or more votes are active for referendum ${index} and wallet ${wallet} at block ${block}`
 }
 
 export function NoOpenVoteFound(block: number, index: number, wallet?: string) {
