53a9faed176f582463887d8316b73f324a5bed97
Gabriel <gabrieljaeger1@gmail.com>
2022-12-08 17:44:47 +0700

    fix missing storage data bug

--- src/mappings/referenda/events/confirmStarted.ts
+++ src/mappings/referenda/events/confirmStarted.ts
@@ -2,6 +2,7 @@ import { BatchContext, SubstrateBlock } from '@subsquid/substrate-processor'
 import { EventItem } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
 import { Store } from '@subsquid/typeorm-store'
 import { OpenGovReferendumStatus } from '../../../model'
+import { getReferendumInfoOf } from '../../../storage/referenda/referendumInfoOf'
 import { updateOpenGovReferendum } from '../../utils/proposals'
 import { getConfirmStartedData } from './getters'
 
@@ -9,5 +10,11 @@ export async function handleConfirmStarted(ctx: BatchContext<Store, unknown>,
     item: EventItem<'Referenda.ConfirmStarted', { event: { args: true; extrinsic: { hash: true } } }>,
     header: SubstrateBlock): Promise<void> {
     const index = getConfirmStartedData(ctx, item.event)
-    await updateOpenGovReferendum(ctx, index, OpenGovReferendumStatus.ConfirmStarted, header)
+    // get referenda data
+    const storageData = await getReferendumInfoOf(ctx, index, header)
+    if (!storageData) {
+        ctx.log.warn(`Storage doesn't exist for referendum at block ${header.height}`)
+        return
+    }
+    await updateOpenGovReferendum(ctx, index, OpenGovReferendumStatus.ConfirmStarted, header, storageData)
 }
